9d47a56ed020706418cf84578dd30b743ecfbcbd
James <56524957+JamesKim1044@users.noreply.github.com>
2023-02-14 18:47:10 +0900

    Update models.py

--- database/models.py
+++ database/models.py
@@ -33,7 +33,31 @@ class Project(Base):
     play = Column(TEXT)
     voices = Column(TEXT)
     frame = Column(TEXT)
-    
+    tld = Column(String(20))
+    background = Column(Integer)
     create_dt = Column(DateTime, default = datetime.datetime.now())
     last_mod_dt = Column(DateTime, default = datetime.datetime.now())
+
+class Metadata(Base):
+    __tablename__ = "metadata"
+    id = Column(Integer, primary_key = True, index = True)
+    project_key = Column(String(10))
+    user_key = Column(String(10))
+    article = Column(String(200), unique = True)
+    journal = Column(String(100))
+    coresspond = Column(String(200))
+    copyright = Column(String(200))
+    doi = Column(String(200))
+    keywords = Column(String(1000))
+    published = Column(String(100))
+    publication = Column(String(100))
     
+    tld = Column(String(100))
+
+    background = Column(Integer)
+
+    authors = Column(TEXT)
+    abstract = Column(TEXT)
+    italics = Column(TEXT)
+    imgs = Column(TEXT)
+    sups = Column(TEXT)
